eee1bdbcb5e1bddc48adfdc531b50d91e6e76592
akgunbusra <busraakgnn@gmail.com>
2018-09-17 16:19:51 +0300

    Add files via upload

---
+++ sketch_jul16c.ino
@@ -0,0 +1,15 @@
+ void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+
+  // prints title with ending line break
+  Serial.println("ASCII Table ~ Character Map");
+}
+
+void loop(){
+  
+  String test = Serial.readString();
+
+    Serial.println(test);
+
+  }
