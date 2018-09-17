075caed84fefc0a0f3f1301df525958f9a6ef49a
akgunbusra <busraakgnn@gmail.com>
2018-09-17 16:20:56 +0300

    Add files via upload

---
+++ sketch_jul17a.ino
@@ -0,0 +1,30 @@
+#include <ISALiquidCrystal.h> //including library
+ISALiquidCrystal lcd;
+long minute, second;
+void setup() {
+  lcd.begin();
+}
+void loop() {
+  minute = millis();
+  second = millis();
+  long temp = minute / 60000;
+  if (temp < 10) {
+    lcd.print(0);
+    lcd.print(temp);
+  }
+  else {
+    lcd.print(temp);
+  }
+  lcd.print(":");
+  long temp2 = second / 1000;
+  temp2 = temp2 % 60;
+  if (temp2 < 10) {
+    lcd.print(0);
+    lcd.print(temp2);
+  }
+  else if (10 <= temp2 <= 60) {
+    lcd.print(temp2);
+  }
+  delay(1000); //1sn
+  lcd.clear();
+}
