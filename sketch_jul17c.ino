1df462757570caff9b32066b80c7ab435007e464
akgunbusra <busraakgnn@gmail.com>
2018-09-17 16:24:05 +0300

    Add files via upload

---
+++ sketch_jul17c.ino
@@ -0,0 +1,25 @@
+#include <ISADefinitions.h> 
+ void setup(){  
+
+  for(int i = 0; i <8 ; i++){
+    pinMode(LEDS[i], OUTPUT);
+  }
+  //digitalWrite(LED1,LOW);
+  } 
+ void loop(){
+ int counter =8;
+ for(int i =0; i<counter; i++){
+  digitalWrite(LEDS[i], HIGH);
+  delay(1000);
+  digitalWrite(LEDS[i],LOW);
+  delay(1000);
+ }  
+for(int i=7; i>=0; i--)
+{
+  digitalWrite(LEDS[i], HIGH);
+  delay(1000);
+  digitalWrite(LEDS[i],LOW);
+  delay(1000);
+  }      
+
+}  
