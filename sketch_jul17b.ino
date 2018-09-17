92dec00ad27689b3df0286835a2893e487fed725
akgunbusra <busraakgnn@gmail.com>
2018-09-17 16:23:26 +0300

    Add files via upload

---
+++ sketch_jul17b.ino
@@ -0,0 +1,12 @@
+unsigned long time=60.000;
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  Serial.print("Time: ");
+  time = millis();
+
+  Serial.println(time);    //prints time since program started
+  delay(1000);             // wait a second so as not to send massive amounts of data
+}
