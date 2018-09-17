b8d1cb53a1e785194080e82db275b52a58306c27
akgunbusra <busraakgnn@gmail.com>
2018-09-17 15:49:10 +0300

    Add files via upload

---
+++ sketch_jul16a.ino
@@ -0,0 +1,18 @@
+#include <ISADefinitions.h>
+#include <ISALiquidCrystal.h>
+
+
+ISALiquidCrystal lcd;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin();
+  lcd.print( "HELLO");
+  //lcd.clear();
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
